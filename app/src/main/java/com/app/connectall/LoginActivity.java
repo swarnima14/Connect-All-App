807435c7cb0c5e0b08853465ba8ab6d96ffbde26
swarnima14 <swarnima@14>
2021-10-27 14:43:50 +0530

    initial

---
+++ app/src/main/java/com/app/connectall/LoginActivity.java
@@ -0,0 +1,91 @@
+package com.app.connectall;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.Html;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    TextInputEditText etMail, etPass;
+    MaterialButton btnLogin;
+    TextView tvRegister, tvForgot;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        initialise();
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String mail = etMail.getText().toString().trim();
+                String pass = etPass.getText().toString().trim();
+                logIn(mail, pass);
+            }
+        });
+        tvRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
+                finish();
+            }
+        });
+        tvForgot.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mAuth.sendPasswordResetEmail(etMail.getText().toString().trim());
+            }
+        });
+    }
+
+    private void initialise() {
+        mAuth = FirebaseAuth.getInstance();
+        etMail = findViewById(R.id.etLoginEmail);
+        etPass = findViewById(R.id.etLoginPass);
+        btnLogin = findViewById(R.id.btnLogin);
+        tvForgot = findViewById(R.id.tvForgot);
+        tvRegister = findViewById(R.id.tvRegister);
+        String t2="<u>Register here.</u>";
+        tvRegister.setText(Html.fromHtml(t2));
+        String t3="<u>Forgot Password?</u>";
+        tvForgot.setText(Html.fromHtml(t3));
+    }
+
+    private void logIn(String email, String password)
+    {
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                            finish();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+
+                        }
+                    }
+                });
+    }
+}
---
+++ app/src/main/java/com/app/connectall/RegisterActivity.java
@@ -0,0 +1,107 @@
+package com.app.connectall;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.Html;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    TextInputEditText etMail, etPass, etConfirmPass;
+    MaterialButton btnReg;
+    TextView tvLogin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        mAuth = FirebaseAuth.getInstance();
+        initialise();
+        btnReg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String mail = etMail.getText().toString().trim();
+                String pass = etPass.getText().toString().trim();
+                if(!mail.contains("iiitu"))
+                {
+                    etMail.requestFocus();
+                    Snackbar.make(v, "Invalid email id." , Snackbar.LENGTH_SHORT).show();
+                }
+                else
+                signIn(mail, pass);
+            }
+        });
+        tvLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
+                finish();
+            }
+        });
+    }
+
+    private void initialise() {
+        etMail = findViewById(R.id.etMail);
+        etPass = findViewById(R.id.etPass);
+        etConfirmPass = findViewById(R.id.etConfirmPass);
+        btnReg = findViewById(R.id.btnRegister);
+        tvLogin = findViewById(R.id.tvLogin);
+        String t2="<u>Login here.</u>";
+        tvLogin.setText(Html.fromHtml(t2));
+    }
+
+    private void signIn(String email, String password)
+    {
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Toast.makeText(RegisterActivity.this, "Auth successful", Toast.LENGTH_SHORT).show();
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            boolean emailVerified = user.isEmailVerified();
+                            if(emailVerified)
+                            {
+                                startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
+                                finish();
+                            }
+
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Toast.makeText(RegisterActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+
+                        }
+                    }
+                });
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+            startActivity(new Intent(this, MainActivity.class));
+            finish();
+        }
+    }
+}
