b6a679eec3a349b18223fd26370124b4dda3b26a
unknown <Ankita2426>
2021-11-02 11:28:20 +0530

    add alumni details

--- app/src/main/java/com/app/connectall/EditProfileActivity.java
+++ app/src/main/java/com/app/connectall/EditProfileActivity.java
@@ -1,6 +1,14 @@
 package com.app.connectall;
 
 import android.app.Activity;
+import android.os.Bundle;
 
-public class EditProfileActivity extends Activity {
+import androidx.appcompat.app.AppCompatActivity;
+
+public class EditProfileActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.act7_alumni_details_continue);
+    }
 }
