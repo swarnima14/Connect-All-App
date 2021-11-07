752415d65e748dee5fed07df5aa67040d9daada8
swarnima14 <swarnima@14>
2021-11-07 18:07:45 +0530

    commit

--- app/src/main/java/com/app/connectall/MainActivity.java
+++ app/src/main/java/com/app/connectall/MainActivity.java
@@ -2,13 +2,65 @@ package com.app.connectall;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.button.MaterialButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    MaterialButton alumni, student;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.act4_selection);
+
+        alumni = findViewById(R.id.btnSelAlumni);
+        student = findViewById(R.id.btnSelStudent);
+        SharedPreferences preferences = getSharedPreferences("TYPE_PREF",MODE_PRIVATE);
+        SharedPreferences.Editor editor = preferences.edit();
+
+
+        alumni.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editor.putString("type", "alumni");
+                editor.apply();
+                startActivity(new Intent(MainActivity.this, EditProfileActivity.class));
+                finish();
+            }
+        });
+
+        student.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editor.putString("type", "student");
+                editor.apply();
+                startActivity(new Intent(MainActivity.this, StudentProfileActivity.class));
+                finish();
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        SharedPreferences preferences = getSharedPreferences("TYPE_PREF",MODE_PRIVATE);
+
+        String type = preferences.getString("type","not defined");
+        if(type.equals("alumni"))
+        {
+            startActivity(new Intent(MainActivity.this, EditProfileActivity.class));
+            finish();
+        }
+        if(type.equals("student"))
+        {
+            startActivity(new Intent(MainActivity.this, StudentProfileActivity.class));
+            finish();
+        }
     }
 }
--- app/src/main/java/com/app/connectall/StudentProfileActivity.java
+++ app/src/main/java/com/app/connectall/StudentProfileActivity.java
@@ -1,20 +1,17 @@
 package com.app.connectall;
 
-import android.app.Activity;
-import android.content.SharedPreferences;
-import android.os.Bundle;
-import android.os.PersistableBundle;
-import android.widget.Toast;
-
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class EditProfileActivity extends AppCompatActivity {
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class StudentProfileActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.act5_alumni_details);
+        setContentView(R.layout.act5_student_profile);
 
         SharedPreferences preferences = getSharedPreferences("TYPE_PREF",MODE_PRIVATE);
         String type = preferences.getString("type", null);
